903adb1c5f2027584fa42373ef62a85914324941
Allen Yeung <ayeung@aptana.com>
2011-07-21 13:56:32 -0700

    Revert "Fix APSTUD-2134:Add .location property to file templates"
    This reverts commit b1d0914153e5a7493afbd98069c9044e8f4d5071.

--- templates/templates.rb
+++ templates/templates.rb
@@ -2,5 +2,9 @@ require 'ruble'
 
 template "CSS Template" do |t|
   t.filetype = "*.css"
-  t.location = "templates/template.css"
+  t.invoke do |context|
+    ENV['TM_DATE'] = Time.now.strftime("%Y-%m-%d")
+    raw_contents = IO.read("#{ENV['TM_BUNDLE_SUPPORT']}/../templates/template.css")
+    raw_contents.gsub(/\$\{([^}]*)\}/) {|match| ENV[match[2..-2]] }
+ end
 end
